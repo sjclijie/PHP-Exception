71aefd4360d557c77b6f6bc68aad9cd145c9e37a
jaylee <sjclijie@vip.qq.com>
2016-03-21 22:44:58 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # PHP-Exception
+
+#### PHP内置的异常类
+
+- Exception
+- ErrorException
+
+#### SPL扩展的异常类
+
+- LogicException
+- BadFunctionCallException
+- BadMethodCallException
+- DomainException
+- InvalidArgumentException
+- LengthException
+- OutOfRangeException
--- all-exception.php
+++ all-exception.php
@@ -5,3 +5,29 @@
  * Date: 16/3/21
  * Time: 21:54
  */
+
+set_exception_handler( function( $e ){
+    var_dump( $e->getMessage() );
+} );
+
+
+try {
+
+//    throw new LogicException('xxx');
+//    throw new LengthException('aaa');
+//    throw new BadFunctionCallException('xx');
+//    throw new BadMethodCallException('xxx');
+
+} catch ( LengthException $e) {
+
+    var_dump( $e->getMessage() );
+
+} finally {
+    //这里的代码始终会运行
+    echo "always do this.";
+}
+
+
+throw new Exception('xcxx');
+
+
